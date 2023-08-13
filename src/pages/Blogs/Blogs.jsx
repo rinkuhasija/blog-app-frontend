ba592daac12a17a40c4e5c6076a28d9190fc9046
Rinku Hasija <rishabhasija5454@gmail.com>
2023-08-14 01:16:40 +0530

    Update Blogs.jsx

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -29,8 +29,8 @@ function Blogs() {
 
             <div className={styles.topTextContainer}>
                 <h1> All Blog Posts</h1> <br />
-                <button onClick={() => navigate("/add-blog")}> Add a Blog Post <span> 
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M19 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-2 10h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"/></svg></span> </button>
+                <button onClick={() => navigate("/add-blog")}> Add a Blog Post 
+                </button>
             </div>
 
 
